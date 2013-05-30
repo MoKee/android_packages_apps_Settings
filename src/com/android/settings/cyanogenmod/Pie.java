c6850883f7315901c30e8bf75234383b5853e666
martincz <martincz@mokeetek.com>
2013-05-30 22:07:44 +0800

    Pie: set angle default to 12° (2/2)
    Change-Id: Id6827561ade45f0af4e35fe4d2eaaa4309fa6705

--- src/com/android/settings/cyanogenmod/Pie.java
+++ src/com/android/settings/cyanogenmod/Pie.java
@@ -143,7 +143,7 @@ public class Pie extends SettingsPreferenceFragment implements OnPreferenceChang
 
         mPieAngle = (ListPreference) prefSet.findPreference(PIE_ANGLE);
         int pieAngle = Settings.System.getInt(getActivity().getContentResolver(),
-                Settings.System.PIE_ANGLE, 0);
+                Settings.System.PIE_ANGLE, 12);
         mPieAngle.setValue(String.valueOf(pieAngle));
         mPieAngle.setOnPreferenceChangeListener(this);
 
