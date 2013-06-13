96944d03496ec5cff2a3c28b03cbf9b1236ade33
martincz <martincz@mokeetek.com>
2013-06-13 15:58:22 +0800

    HALO: Option to pause the underlying app (2/2)
    - adds an option to pause the active app when using HALO
    - this is enabled by default for devices with low memory
    - (currently: <640MB RAM available to the kernel)
    
    Change-Id: Id9ec776444e2d10196fb8d5cee620e7c303265e1

--- src/com/android/settings/cyanogenmod/Halo.java
+++ src/com/android/settings/cyanogenmod/Halo.java
@@ -1,5 +1,6 @@
 package com.android.settings.cyanogenmod;
 
+import android.app.ActivityManager;
 import android.app.INotificationManager;
 import android.content.Context;
 import android.os.Bundle;
@@ -21,10 +22,12 @@ public class Halo extends SettingsPreferenceFragment implements OnPreferenceChan
     private static final String KEY_HALO_STATE = "halo_state";
     private static final String KEY_HALO_HIDE = "halo_hide";
     private static final String KEY_HALO_REVERSED = "halo_reversed";
+    private static final String KEY_HALO_PAUSE = "halo_pause";
 
     private ListPreference mHaloState;
     private CheckBoxPreference mHaloHide;
     private CheckBoxPreference mHaloReversed;
+    private CheckBoxPreference mHaloPause;
 
     private Context mContext;
     private INotificationManager mNotificationManager;
@@ -52,6 +55,11 @@ public class Halo extends SettingsPreferenceFragment implements OnPreferenceChan
         mHaloReversed.setChecked(Settings.System.getInt(mContext.getContentResolver(),
                 Settings.System.HALO_REVERSED, 1) == 1);
 
+        int isLowRAM = (ActivityManager.isLargeRAM()) ? 0 : 1;
+        mHaloPause = (CheckBoxPreference) prefSet.findPreference(KEY_HALO_PAUSE);
+        mHaloPause.setChecked(Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.HALO_PAUSE, isLowRAM) == 1);
+
     }
 
     private boolean isHaloPolicyBlack() {
@@ -72,6 +80,10 @@ public class Halo extends SettingsPreferenceFragment implements OnPreferenceChan
         } else if (preference == mHaloReversed) {	
             Settings.System.putInt(mContext.getContentResolver(),
                     Settings.System.HALO_REVERSED, mHaloReversed.isChecked()
+                    ? 1 : 0);
+        } else if (preference == mHaloPause) {
+            Settings.System.putInt(mContext.getContentResolver(),
+                    Settings.System.HALO_PAUSE, mHaloPause.isChecked()
                     ? 1 : 0);
         }
         return super.onPreferenceTreeClick(preferenceScreen, preference);
