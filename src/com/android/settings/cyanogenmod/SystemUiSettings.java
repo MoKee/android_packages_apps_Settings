27440f7af38e334ed966202b631ed0d06e4f97d4
martincz <martincz@mokeetek.com>
2013-08-15 00:10:39 +0800

    Settings: merge fix
    Change-Id: I09bd9ae6c89007585998a80a53c5e2ff5cb8ea19

--- src/com/android/settings/cyanogenmod/SystemUiSettings.java
+++ src/com/android/settings/cyanogenmod/SystemUiSettings.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2012 The CyanogenMod project
+ * Copyright (C) 2012 The MoKee OpenSource project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,7 +17,10 @@
 
 package com.android.settings.cyanogenmod;
 
+import android.app.AlertDialog;
 import android.content.ContentResolver;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.os.RemoteException;
@@ -25,24 +29,45 @@ import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceScreen;
 import android.provider.Settings;
+import android.text.Spannable;
 import android.util.Log;
 import android.view.WindowManagerGlobal;
+import android.widget.EditText;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.widget.AdvancedTransparencyDialog;
+import com.android.settings.widget.ColorPickerPreference;
 
-public class SystemUiSettings extends SettingsPreferenceFragment  implements
+public class SystemUiSettings extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
     private static final String TAG = "SystemSettings";
 
+    private static final String KEY_CUSTOM_CARRIER_LABEL = "custom_carrier_label";
     private static final String KEY_EXPANDED_DESKTOP = "expanded_desktop";
     private static final String KEY_EXPANDED_DESKTOP_NO_NAVBAR = "expanded_desktop_no_navbar";
-    private static final String CATEGORY_NAVBAR = "navigation_bar";
-    private static final String KEY_PIE_CONTROL = "pie_control";
+    private static final String KEY_FULLSCREEN_KEYBOARD = "fullscreen_keyboard";
+    private static final String KEY_LISTVIEW_ANIMATION = "listview_animation";
+    private static final String KEY_LISTVIEW_INTERPOLATOR = "listview_interpolator";
+    private static final String KEY_MMS_BREATH = "mms_breath";
+    private static final String KEY_MISSED_CALL_BREATH = "missed_call_breath";
+    private static final String KEY_NAVIGATION_BAR_HEIGHT = "navigation_bar_height";
+    private static final String KEY_NAVBAR_COLOR = "nav_bar_color";
 
-    private PreferenceScreen mPieControl;
+    private static final String CATEGORY_NAVBAR = "navigation_bar";
+
+    private String mCustomLabelText;
+
     private ListPreference mExpandedDesktopPref;
+    private ListPreference mListViewAnimation;
+    private ListPreference mListViewInterpolator;
+    private ListPreference mNavButtonsHeight;
     private CheckBoxPreference mExpandedDesktopNoNavbarPref;
+    private CheckBoxPreference mFullscreenKeyboard;
+    private CheckBoxPreference mMMSBreath;
+    private CheckBoxPreference mMissedCallBreath;
+    private ColorPickerPreference mNavigationBarColor;
+    private PreferenceScreen mCustomLabel;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -51,7 +76,47 @@ public class SystemUiSettings extends SettingsPreferenceFragment  implements
         addPreferencesFromResource(R.xml.system_ui_settings);
         PreferenceScreen prefScreen = getPreferenceScreen();
 
-        mPieControl = (PreferenceScreen) findPreference(KEY_PIE_CONTROL);
+        mNavButtonsHeight = (ListPreference) findPreference(KEY_NAVIGATION_BAR_HEIGHT);
+        mNavButtonsHeight.setOnPreferenceChangeListener(this);
+
+        int statusNavButtonsHeight = Settings.System.getInt(getActivity().getContentResolver(),
+                Settings.System.NAVIGATION_BAR_HEIGHT, 48);
+        mNavButtonsHeight.setValue(String.valueOf(statusNavButtonsHeight));
+        mNavButtonsHeight.setSummary(mNavButtonsHeight.getEntry());
+
+        mFullscreenKeyboard = (CheckBoxPreference) findPreference(KEY_FULLSCREEN_KEYBOARD);
+        mFullscreenKeyboard.setOnPreferenceChangeListener(this);
+        mFullscreenKeyboard.setChecked(Settings.System.getInt(getActivity().getApplicationContext().getContentResolver(),
+                Settings.System.FULLSCREEN_KEYBOARD, 0) == 1);
+        mMMSBreath = (CheckBoxPreference) findPreference(KEY_MMS_BREATH);
+        mMMSBreath.setOnPreferenceChangeListener(this);
+        mMMSBreath.setChecked(Settings.System.getInt(getActivity().getApplicationContext().getContentResolver(),
+                Settings.System.MMS_BREATH, 0) == 1);
+        mMissedCallBreath = (CheckBoxPreference) findPreference(KEY_MISSED_CALL_BREATH);
+        mMissedCallBreath.setOnPreferenceChangeListener(this);
+        mMissedCallBreath.setChecked(Settings.System.getInt(getActivity().getApplicationContext().getContentResolver(),
+                Settings.System.MISSED_CALL_BREATH, 0) == 1);
+
+        //ListView Animations
+        mListViewAnimation = (ListPreference) findPreference(KEY_LISTVIEW_ANIMATION);
+        int listviewanimation = Settings.System.getInt(getActivity().getContentResolver(),
+            Settings.System.LISTVIEW_ANIMATION, 1);
+        mListViewAnimation.setValue(String.valueOf(listviewanimation));
+        mListViewAnimation.setSummary(mListViewAnimation.getEntry());
+        mListViewAnimation.setOnPreferenceChangeListener(this);
+
+        mListViewInterpolator = (ListPreference) findPreference(KEY_LISTVIEW_INTERPOLATOR);
+        int listviewinterpolator = Settings.System.getInt(getActivity().getContentResolver(),
+            Settings.System.LISTVIEW_INTERPOLATOR, 0);
+        mListViewInterpolator.setValue(String.valueOf(listviewinterpolator));
+        mListViewInterpolator.setSummary(mListViewInterpolator.getEntry());
+        mListViewInterpolator.setOnPreferenceChangeListener(this);
+
+        mNavigationBarColor = (ColorPickerPreference) findPreference(KEY_NAVBAR_COLOR);
+        mNavigationBarColor.setOnPreferenceChangeListener(this);
+
+        mCustomLabel = (PreferenceScreen) findPreference(KEY_CUSTOM_CARRIER_LABEL);
+        updateCustomLabelTextSummary();
 
         // Expanded desktop
         mExpandedDesktopPref = (ListPreference) findPreference(KEY_EXPANDED_DESKTOP);
@@ -79,7 +144,6 @@ public class SystemUiSettings extends SettingsPreferenceFragment  implements
             // Hide navigation bar category on devices without navigation bar
             if (!hasNavBar) {
                 prefScreen.removePreference(findPreference(CATEGORY_NAVBAR));
-                mPieControl = null;
             }
         } catch (RemoteException e) {
             Log.e(TAG, "Error getting navigation bar status");
@@ -89,10 +153,11 @@ public class SystemUiSettings extends SettingsPreferenceFragment  implements
     @Override
     public void onResume() {
         super.onResume();
-        updatePieControlSummary();
+        updatemExpandedDesktopDescription();
     }
 
     public boolean onPreferenceChange(Preference preference, Object objValue) {
+        ContentResolver resolver = getActivity().getApplicationContext().getContentResolver();
         if (preference == mExpandedDesktopPref) {
             int expandedDesktopValue = Integer.valueOf((String) objValue);
             updateExpandedDesktop(expandedDesktopValue);
@@ -101,22 +166,51 @@ public class SystemUiSettings extends SettingsPreferenceFragment  implements
             boolean value = (Boolean) objValue;
             updateExpandedDesktop(value ? 2 : 0);
             return true;
+        } else if (preference == mNavButtonsHeight) {
+            int index = mNavButtonsHeight.findIndexOfValue((String) objValue);
+            Settings.System.putInt(resolver, Settings.System.NAVIGATION_BAR_HEIGHT, 
+                    Integer.valueOf((String) objValue));
+            mNavButtonsHeight.setSummary(mNavButtonsHeight.getEntries()[index]);
+            return true;
+        } else if (preference == mNavigationBarColor) {
+            String hex = ColorPickerPreference.convertToARGB(
+                    Integer.valueOf(String.valueOf(objValue)));
+            preference.setSummary(hex);
+            int intHex = ColorPickerPreference.convertToColorInt(hex) & 0x00FFFFFF;
+            Settings.System.putInt(resolver,
+                    Settings.System.NAVIGATION_BAR_COLOR, intHex);
+            return true;
+        } else if (preference == mFullscreenKeyboard) {
+            Settings.System.putInt(resolver, Settings.System.FULLSCREEN_KEYBOARD,
+                    ((Boolean) objValue).booleanValue() ? 1 : 0);
+            return true;
+        } else if (preference == mMMSBreath) {
+            Settings.System.putInt(resolver, Settings.System.MMS_BREATH, 
+                    ((Boolean) objValue).booleanValue() ? 1 : 0);
+            return true;
+        } else if (preference == mMissedCallBreath) {
+            Settings.System.putInt(resolver, Settings.System.MISSED_CALL_BREATH, 
+                    ((Boolean) objValue).booleanValue() ? 1 : 0);
+            return true;
+        } else if (preference == mListViewAnimation) {
+            int listviewanimation = Integer.valueOf((String) objValue);
+            int index = mListViewAnimation.findIndexOfValue((String) objValue);
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.LISTVIEW_ANIMATION,
+                    listviewanimation);
+            mListViewAnimation.setSummary(mListViewAnimation.getEntries()[index]);
+            return true;
+        } else if (preference == mListViewInterpolator) {
+            int listviewinterpolator = Integer.valueOf((String) objValue);
+            int index = mListViewInterpolator.findIndexOfValue((String) objValue);
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.LISTVIEW_INTERPOLATOR,
+                    listviewinterpolator);
+            mListViewInterpolator.setSummary(mListViewInterpolator.getEntries()[index]);
+            return true; 
         }
 
         return false;
-    }
-
-    private void updatePieControlSummary() {
-        if (mPieControl != null) {
-            boolean enabled = Settings.System.getInt(getContentResolver(),
-                Settings.System.PIE_CONTROLS, 0) != 0;
-
-            if (enabled) {
-                mPieControl.setSummary(R.string.pie_control_enabled);
-            } else {
-                mPieControl.setSummary(R.string.pie_control_disabled);
-            }
-        }
     }
 
     private void updateExpandedDesktop(int value) {
@@ -142,5 +236,86 @@ public class SystemUiSettings extends SettingsPreferenceFragment  implements
         if (mExpandedDesktopPref != null && summary != -1) {
             mExpandedDesktopPref.setSummary(res.getString(summary));
         }
+    }
+
+    private void updateCustomLabelTextSummary() { 
+        mCustomLabelText = Settings.System.getString(getActivity().getContentResolver(),
+                Settings.System.CUSTOM_CARRIER_LABEL);
+    
+        if (mCustomLabelText == null || mCustomLabelText.length() == 0) {
+            mCustomLabel.setSummary(R.string.custom_carrier_label_notset);
+        } else {
+            mCustomLabel.setSummary(mCustomLabelText);
+      
+        }
+    }
+
+    private void openTransparencyDialog() {
+        getFragmentManager().beginTransaction().add(new AdvancedTransparencyDialog(), null)
+                .commit();
+    }
+
+    private void updatemExpandedDesktopDescription() {
+        int expandedDesktopValue = Settings.System.getInt(getContentResolver(),
+                Settings.System.EXPANDED_DESKTOP_STYLE, 0);
+        try {
+        if (WindowManagerGlobal.getWindowManagerService().hasNavigationBar()) {
+            switch (expandedDesktopValue) {
+                case 0:
+                mExpandedDesktopPref.setSummary(R.string.expanded_desktop_disabled);
+                break;
+                case 1:
+                mExpandedDesktopPref.setSummary(R.string.expanded_desktop_status_bar);
+                break;
+                case 2:
+                mExpandedDesktopPref.setSummary(R.string.expanded_desktop_no_status_bar);
+                break;
+            }
+                mExpandedDesktopPref.setValue(String.valueOf(expandedDesktopValue));
+        } else {
+            mExpandedDesktopNoNavbarPref.setChecked(expandedDesktopValue > 0);
+        }
+        } catch (RemoteException e) {
+            Log.e(TAG, "Error getting navigation bar status");
+        }
+    }
+
+    @Override
+    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen,
+            final Preference preference) {
+        if (preference.getKey().equals("transparency_dialog")) {
+            openTransparencyDialog();
+            return true;
+        } else if (preference.getKey().equals(KEY_CUSTOM_CARRIER_LABEL)) {
+            AlertDialog.Builder alert = new AlertDialog.Builder(getActivity());
+            alert.setTitle(R.string.custom_carrier_label_title);
+            alert.setMessage(R.string.custom_carrier_label_explain);
+
+            // Set an EditText view to get user input
+            final EditText input = new EditText(getActivity());
+            input.setText(mCustomLabelText != null ? mCustomLabelText : "");
+            alert.setView(input);
+            alert.setPositiveButton(getResources().getString(R.string.ok),
+                    new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int whichButton) {
+                    String value = ((Spannable) input.getText()).toString();
+                    Settings.System.putString(getActivity().getContentResolver(),
+                            Settings.System.CUSTOM_CARRIER_LABEL, value);
+                    updateCustomLabelTextSummary();
+                    Intent i = new Intent();
+                    i.setAction("com.android.settings.LABEL_CHANGED");
+                    getActivity().sendBroadcast(i);
+                }
+            });
+            alert.setNegativeButton(getResources().getString(R.string.cancel),
+                    new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int whichButton) {
+                    // Canceled.
+                }
+            });
+
+            alert.show();
+        } 
+        return super.onPreferenceTreeClick(preferenceScreen, preference);
     }
 }
