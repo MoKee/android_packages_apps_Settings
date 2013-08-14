363bca5afe8aaf5b0351dc1ec63ccdf55b81a8e5
martincz <martincz@mokeetek.com>
2013-08-14 23:33:28 +0800

    Settings: fix merge
    Change-Id: If2d5996651fc2bca40363e6dc274b8ab69863f45

--- src/com/android/settings/cyanogenmod/ButtonSettings.java
+++ src/com/android/settings/cyanogenmod/ButtonSettings.java
@@ -61,6 +61,7 @@ public class ButtonSettings extends SettingsPreferenceFragment implements
     private static final int ACTION_SEARCH = 3;
     private static final int ACTION_VOICE_SEARCH = 4;
     private static final int ACTION_IN_APP_SEARCH = 5;
+    private static final int ACTION_SLEEP = 6;
 
     // Masks for checking presence of hardware keys.
     // Must match values in frameworks/base/core/res/res/values/config.xml
