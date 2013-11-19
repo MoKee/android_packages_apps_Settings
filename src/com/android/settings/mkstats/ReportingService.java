25fe825dc607a0d2d6d57b93c3d07e7e1ffa2f4e
martincz <martincz@mokeetek.com>
2013-11-19 19:23:34 +0800

    Settings: mokee stats client update
    Change-Id: Id5197d8cc3671b9cb892044589638154d20e068d

--- src/com/android/settings/mkstats/ReportingService.java
+++ src/com/android/settings/mkstats/ReportingService.java
@@ -32,39 +32,23 @@ import org.apache.http.message.BasicNameValuePair;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import android.app.Notification;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
 import android.app.Service;
-import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.IBinder;
 import android.util.Log;
 
+import com.android.settings.R;
 import com.google.analytics.tracking.android.GoogleAnalytics;
 import com.google.analytics.tracking.android.Tracker;
-import com.android.settings.R;
-import com.android.settings.Settings;
 
 public class ReportingService extends Service {
     protected static final String TAG = ReportingService.class.getSimpleName();
-    
-    protected static final String ANONYMOUS_PREF = "mokee_stats";
 
-    protected static final String ANONYMOUS_ALARM_SET = "pref_anonymous_alarm_set";
-	
-    protected static final String ANONYMOUS_CHECK_LOCK = "pref_anonymous_check_lock";
+    private StatsUploadTask mTask;
 
-    protected static final String ANONYMOUS_FIRST_BOOT = "pref_anonymous_first_boot";
-
-    protected static final String ANONYMOUS_FLASH_TIME = "pref_anonymous_flash_time";
-	
-    protected static final String ANONYMOUS_LAST_CHECKED = "pref_anonymous_checked_in";
-
-    protected static final String DEVICE_MOKEE_VERSION = "pref_device_mokee_version";
-	
     @Override
     public IBinder onBind(Intent intent) {
         return null;
@@ -72,91 +56,120 @@ public class ReportingService extends Service {
 
     @Override
     public int onStartCommand (Intent intent, int flags, int startId) {
-        SharedPreferences prefs =  getSharedPreferences(ANONYMOUS_PREF, 0);
-        if (!prefs.getBoolean(ANONYMOUS_CHECK_LOCK, false)) {
-		prefs.edit().putBoolean(ANONYMOUS_CHECK_LOCK, true).apply();
-		Log.d(TAG, "User has opted in -- reporting.");
-		Thread thread = new Thread() {
-			@Override
-		        public void run() {
-				report();
-		        }
-		};
-		thread.start();
-	}
+        Log.d(TAG, "User has opted in -- reporting.");
+
+        if (mTask == null || mTask.getStatus() == AsyncTask.Status.FINISHED) {
+            mTask = new StatsUploadTask();
+            mTask.execute();
+        }
+
         return Service.START_REDELIVER_INTENT;
     }
 
-    private void report() {
-        final Context context = ReportingService.this;
-        String deviceId = Utilities.getUniqueID(context);
-        String deviceName = Utilities.getDevice();
-        String deviceVersion = Utilities.getModVersion();
-        String deviceCountry = Utilities.getCountryCode(context);
-        String deviceCarrier = Utilities.getCarrier(context);
-        String deviceCarrierId = Utilities.getCarrierId(context);
-        String deviceMoKeeVersion = Utilities.getMoKeeVersion();
+    private class StatsUploadTask extends AsyncTask<Void, Void, Boolean> {
 
-        Log.d(TAG, "SERVICE: Device ID=" + deviceId);
-        Log.d(TAG, "SERVICE: Device Name=" + deviceName);
-        Log.d(TAG, "SERVICE: Device Version=" + deviceVersion);
-        Log.d(TAG, "SERVICE: Country=" + deviceCountry);
-        Log.d(TAG, "SERVICE: Carrier=" + deviceCarrier);
-        Log.d(TAG, "SERVICE: Carrier ID=" + deviceCarrierId);
+        private InputStream is;
 
-        // report to google analytics
-        GoogleAnalytics ga = GoogleAnalytics.getInstance(this);
-        Tracker tracker = ga.getTracker(getString(R.string.ga_trackingId));
-        tracker.sendEvent(deviceName, deviceVersion, deviceCountry, null);
-        // this really should be set at build time...
-        // format of version should be:
-        // version[-date-type]-device
-        String[] parts = deviceVersion.split("-");
-        String deviceVersionNoDevice = null;
-        if (parts.length == 2) {
-            deviceVersionNoDevice = parts[0];
-        }
-        else if (parts.length == 4) {
-            deviceVersionNoDevice = parts[0] + "-" + parts[2];
-        }
-        if (deviceVersionNoDevice != null)
-            tracker.sendEvent("checkin", deviceName, deviceVersionNoDevice, null);
-        tracker.close();
+        @Override
+        protected Boolean doInBackground(Void... params) {
+            final Context context = ReportingService.this;
+            String deviceId = Utilities.getUniqueID(context);
+            String deviceName = Utilities.getDevice();
+            String deviceVersion = Utilities.getModVersion();
+            String deviceCountry = Utilities.getCountryCode(context);
+            String deviceCarrier = Utilities.getCarrier(context);
+            String deviceCarrierId = Utilities.getCarrierId(context);
 
-        // report to the cmstats service
-        HttpClient httpclient = new DefaultHttpClient();
-        HttpPost httppost = new HttpPost("http://stats.mfunz.com/index.php/Submit/flash");
-		SharedPreferences prefs =  getSharedPreferences(ANONYMOUS_PREF, 0);
-        try {
-            List<NameValuePair> kv = new ArrayList<NameValuePair>(5);
-            kv.add(new BasicNameValuePair("device_hash", deviceId));
-            kv.add(new BasicNameValuePair("device_name", deviceName));
-            kv.add(new BasicNameValuePair("device_version", deviceVersion));
-            kv.add(new BasicNameValuePair("device_country", deviceCountry));
-            kv.add(new BasicNameValuePair("device_carrier", deviceCarrier));
-            kv.add(new BasicNameValuePair("device_carrier_id", deviceCarrierId));
-            httppost.setEntity(new UrlEncodedFormEntity(kv));
-            InputStream is = httpclient.execute(httppost).getEntity().getContent();
-            long device_flash_time = Long.valueOf(convertStreamToJSONObject(is).getString("device_flash_time"));
-            prefs.edit().putLong(ANONYMOUS_LAST_CHECKED,
-                    System.currentTimeMillis()).putLong(ANONYMOUS_FLASH_TIME,
-                                device_flash_time).putBoolean(ANONYMOUS_FIRST_BOOT, false).putBoolean(ANONYMOUS_CHECK_LOCK, false).putString(DEVICE_MOKEE_VERSION, deviceMoKeeVersion).apply();
-        } catch (Exception e) {
-            Log.e(TAG, "Got Exception", e);
-			prefs.edit().putBoolean(ANONYMOUS_CHECK_LOCK, false).apply();
+            Log.d(TAG, "SERVICE: Device ID=" + deviceId);
+            Log.d(TAG, "SERVICE: Device Name=" + deviceName);
+            Log.d(TAG, "SERVICE: Device Version=" + deviceVersion);
+            Log.d(TAG, "SERVICE: Country=" + deviceCountry);
+            Log.d(TAG, "SERVICE: Carrier=" + deviceCarrier);
+            Log.d(TAG, "SERVICE: Carrier ID=" + deviceCarrierId);
+
+            // report to google analytics
+            GoogleAnalytics ga = GoogleAnalytics.getInstance(context);
+            Tracker tracker = ga.getTracker(getString(R.string.ga_trackingId));
+            tracker.sendEvent(deviceName, deviceVersion, deviceCountry, null);
+
+            // this really should be set at build time...
+            // format of version should be:
+            // version[-date-type]-device
+            String[] parts = deviceVersion.split("-");
+            String deviceVersionNoDevice = null;
+            if (parts.length == 2) {
+                deviceVersionNoDevice = parts[0];
+            } else if (parts.length == 4) {
+                deviceVersionNoDevice = parts[0] + "-" + parts[2];
+            }
+
+            if (deviceVersionNoDevice != null) {
+                tracker.sendEvent("checkin", deviceName, deviceVersionNoDevice, null);
+            }
+            tracker.close();
+
+            // report to the mkstats service
+            HttpClient httpClient = new DefaultHttpClient();
+            HttpPost httpPost = new HttpPost("http://stats.mfunz.com/index.php/Submit/flash");
+            boolean success = false;
+
+            try {
+                List<NameValuePair> kv = new ArrayList<NameValuePair>(5);
+                kv.add(new BasicNameValuePair("device_hash", deviceId));
+                kv.add(new BasicNameValuePair("device_name", deviceName));
+                kv.add(new BasicNameValuePair("device_version", deviceVersion));
+                kv.add(new BasicNameValuePair("device_country", deviceCountry));
+                kv.add(new BasicNameValuePair("device_carrier", deviceCarrier));
+                kv.add(new BasicNameValuePair("device_carrier_id", deviceCarrierId));
+
+                httpPost.setEntity(new UrlEncodedFormEntity(kv));
+                is = httpClient.execute(httpPost).getEntity().getContent();
+
+                success = true;
+            } catch (IOException e) {
+                Log.w(TAG, "Could not upload stats checkin", e);
+            }
+
+            return success;
         }
-        ReportingServiceManager.setAlarm(this);
-        stopSelf();
+
+        @Override
+        protected void onPostExecute(Boolean result) {
+            final Context context = ReportingService.this;
+            long interval;
+
+            if (result) {
+                String deviceMoKeeVersion = Utilities.getMoKeeVersion();
+                final SharedPreferences prefs = getSharedPreferences(ReportingServiceManager.ANONYMOUS_PREF, 0);
+                long device_flash_time = 0;
+                try {
+                    device_flash_time = Long.valueOf(convertStreamToJSONObject(is).getString("device_flash_time"));
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                prefs.edit().putLong(ReportingServiceManager.ANONYMOUS_LAST_CHECKED,
+                        System.currentTimeMillis()).putLong(ReportingServiceManager.ANONYMOUS_FLASH_TIME,
+                                    device_flash_time).putString(ReportingServiceManager.DEVICE_MOKEE_VERSION, deviceMoKeeVersion).apply();
+                // use set interval
+                interval = 0;
+            } else {
+                // error, try again in 3 hours
+                interval = 3L * 60L * 60L * 1000L;
+            }
+
+            ReportingServiceManager.setAlarm(context, interval);
+            stopSelf();
+        }
     }
 
-	private JSONObject convertStreamToJSONObject(InputStream is) throws IOException, JSONException {
-		StringBuilder builder = new StringBuilder();
-		BufferedReader reader = new BufferedReader(new InputStreamReader(is));
-		for(String str = reader.readLine();str != null; str= reader.readLine())
-		{
-			builder.append(str);
-		}
-		return new JSONObject(builder.toString()); 
-		
-	}
+    private JSONObject convertStreamToJSONObject(InputStream is) throws IOException, JSONException {
+        StringBuilder builder = new StringBuilder();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+        for (String str = reader.readLine(); str != null; str = reader.readLine())
+        {
+            builder.append(str);
+        }
+        return new JSONObject(builder.toString());
+
+    }
 }
--- src/com/android/settings/mkstats/ReportingServiceManager.java
+++ src/com/android/settings/mkstats/ReportingServiceManager.java
@@ -19,88 +19,93 @@ package com.android.settings.mkstats;
 import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
-import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
-import android.os.Handler;
 import android.util.Log;
 
 public class ReportingServiceManager extends BroadcastReceiver {
 
     protected static final String TAG = ReportingServiceManager.class.getSimpleName();
-    
-    public static final long dMill = 24 * 60 * 60 * 1000;
-    public static final long tFrame = 7 * dMill;
+
+    protected static final String ANONYMOUS_PREF = "mokee_stats";
+
+    protected static final String ANONYMOUS_ALARM_SET = "pref_anonymous_alarm_set";
+
+    protected static final String ANONYMOUS_FLASH_TIME = "pref_anonymous_flash_time";
+
+    protected static final String ANONYMOUS_LAST_CHECKED = "pref_anonymous_checked_in";
+
+    protected static final String DEVICE_MOKEE_VERSION = "pref_device_mokee_version";
+
+    private static final long MILLIS_PER_HOUR = 60L * 60L * 1000L;
+    private static final long MILLIS_PER_DAY = 24L * MILLIS_PER_HOUR;
+    private static final long UPDATE_INTERVAL = 1L * MILLIS_PER_DAY;
 
     @Override
     public void onReceive(Context ctx, Intent intent) {
         if (intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED)) {
-            setAlarm(ctx);
+            setAlarm(ctx, 0);
         } else {
             launchService(ctx);
         }
     }
 
-    protected static void setAlarm (Context ctx) {
-        SharedPreferences prefs = ctx.getSharedPreferences(ReportingService.ANONYMOUS_PREF, 0);
-        boolean firstBoot = prefs.getBoolean(ReportingService.ANONYMOUS_FIRST_BOOT, true);
-        if (firstBoot) {
-            return;
+    public static void setAlarm (Context ctx, long millisFromNow) {
+        SharedPreferences prefs = ctx.getSharedPreferences(ANONYMOUS_PREF, 0);
+
+        if (millisFromNow <= 0) {
+            long lastSynced = prefs.getLong(ANONYMOUS_LAST_CHECKED, 0);
+            if (lastSynced == 0) {
+                // never synced, so let's fake out that the last sync was just now.
+                // this will allow the user tFrame time to opt out before it will start
+                // sending up anonymous stats.
+                lastSynced = System.currentTimeMillis();
+                prefs.edit().putLong(ANONYMOUS_LAST_CHECKED, lastSynced).apply();
+                Log.d(ReportingService.TAG, "Set alarm for first sync.");
+            }
+            millisFromNow = (lastSynced + UPDATE_INTERVAL) - System.currentTimeMillis();
         }
-        long lastSynced = prefs.getLong(ReportingService.ANONYMOUS_LAST_CHECKED, 0);
-        if (lastSynced == 0) {
-            return;
-        }
-        long timeLeft = (lastSynced + tFrame) - System.currentTimeMillis();
-        Intent sIntent = new Intent(ConnectivityManager.CONNECTIVITY_ACTION);
-        sIntent.setComponent(new ComponentName(ctx.getPackageName(), ReportingServiceManager.class.getName()));
+
+        Intent intent = new Intent(ConnectivityManager.CONNECTIVITY_ACTION);
+        intent.setClass(ctx, ReportingServiceManager.class);
+
         AlarmManager alarmManager = (AlarmManager) ctx.getSystemService(Context.ALARM_SERVICE);
-        alarmManager.set(AlarmManager.RTC_WAKEUP, System.currentTimeMillis() + timeLeft, PendingIntent.getBroadcast(ctx, 0, sIntent, 0));
-        Log.d(TAG, "Next sync attempt in : " + timeLeft / dMill + " days");
+        alarmManager.set(AlarmManager.RTC_WAKEUP, System.currentTimeMillis() + millisFromNow,
+            PendingIntent.getBroadcast(ctx, 0, intent, 0));
+        Log.d(ReportingService.TAG, "Next sync attempt in : " + millisFromNow / MILLIS_PER_HOUR + " hours");
     }
 
     public static void launchService (Context ctx) {
         ConnectivityManager cm = (ConnectivityManager) ctx.getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo networkInfo = cm.getActiveNetworkInfo();
         if (networkInfo != null && networkInfo.isConnected()) {
-            final SharedPreferences prefs = ctx.getSharedPreferences(ReportingService.ANONYMOUS_PREF, 0);
-            long lastSynced = prefs.getLong(ReportingService.ANONYMOUS_LAST_CHECKED, 0);
-            boolean firstBoot = prefs.getBoolean(ReportingService.ANONYMOUS_FIRST_BOOT, true);
-            boolean checklock = prefs.getBoolean(ReportingService.ANONYMOUS_CHECK_LOCK, false);
+            final SharedPreferences prefs = ctx.getSharedPreferences(ANONYMOUS_PREF, 0);
+            long lastSynced = prefs.getLong(ANONYMOUS_LAST_CHECKED, 0);
             String deviceMoKeeVersion = Utilities.getMoKeeVersion();
-            String prefDeviceMoKeeVersion = prefs.getString(ReportingService.DEVICE_MOKEE_VERSION, deviceMoKeeVersion);
+            String prefDeviceMoKeeVersion = prefs.getString(DEVICE_MOKEE_VERSION, deviceMoKeeVersion);
 
             boolean shouldSync = false;
             if (lastSynced == 0) {
                 shouldSync = true;
-            } else if (System.currentTimeMillis() - lastSynced >= tFrame) {
+            } else if (System.currentTimeMillis() - lastSynced >= UPDATE_INTERVAL) {
                 shouldSync = true;
             } else if (!deviceMoKeeVersion.equals(prefDeviceMoKeeVersion)) {
                 shouldSync = true;
             }
-            if (shouldSync || firstBoot) {
-				if(prefs.getLong(ReportingService.ANONYMOUS_FLASH_TIME, 0) == 0 && !checklock) {
-	                		Intent sIntent = new Intent();
-	                		sIntent.setComponent(new ComponentName(ctx.getPackageName(), ReportingService.class.getName()));
-	                		ctx.startService(sIntent);
-					new Handler().postDelayed(new Runnable(){
-
-					    @Override
-					    public void run() {
-						prefs.edit().putBoolean(ReportingService.ANONYMOUS_CHECK_LOCK, false).apply();
-					    }}, 1000 * 30);
-				}
-				else if(prefs.getLong(ReportingService.ANONYMOUS_FLASH_TIME, 0) != 0) {
-	                		Intent sIntent = new Intent();
-	                		sIntent.setComponent(new ComponentName(ctx.getPackageName(), UpdatingService.class.getName()));
-	                		ctx.startService(sIntent);
-				}
-                
+            if (shouldSync) {
+                Intent sIntent = new Intent();
+                if (prefs.getLong(ANONYMOUS_FLASH_TIME, 0) == 0) {
+                    sIntent.setClass(ctx, ReportingService.class);
+                    ctx.startService(sIntent);
+                } else {
+                    sIntent.setClass(ctx, UpdatingService.class);
+                    ctx.startService(sIntent);
+                }
             } else {
-                setAlarm(ctx);
+                setAlarm(ctx, 0);
             }
         }
     }
