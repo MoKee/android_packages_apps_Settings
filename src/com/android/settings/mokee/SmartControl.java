3082abed1c049d18ebce6e8e35530d1dac251cf4
martincz <martincz@mokeedev.com>
2015-01-23 04:12:49 +0800

    Settings: Direct Call (2/4)
    Change-Id: I0c4e166ed205cc1f64334aa964ceea68c18c2db5

--- src/com/android/settings/mokee/SmartControl.java
+++ src/com/android/settings/mokee/SmartControl.java
@@ -16,23 +16,61 @@
 
 package com.android.settings.mokee;
 
+import android.content.ContentResolver;
 import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.SwitchPreference;
+import android.preference.Preference.OnPreferenceChangeListener;
+import android.provider.Settings;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 
-public class SmartControl extends SettingsPreferenceFragment {
+public class SmartControl extends SettingsPreferenceFragment implements OnPreferenceChangeListener {
 
     private static final String TAG = "SmartControl";
+
+    private static final String DIRECT_CALL_FOR_DIALER = "direct_call_for_dialer";
+    private static final String DIRECT_CALL_FOR_MMS = "direct_call_for_mms";
+
+    private SwitchPreference mDirectCallForDialer;
+    private SwitchPreference mDirectCallForMms;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.smartcontrol_settings);
+
+        ContentResolver resolver = getActivity().getContentResolver();
+
+        mDirectCallForDialer = (SwitchPreference) findPreference(DIRECT_CALL_FOR_DIALER);
+        mDirectCallForDialer.setChecked((Settings.System.getInt(resolver,
+                Settings.System.DIRECT_CALL_FOR_DIALER, 0) == 1));
+        mDirectCallForDialer.setOnPreferenceChangeListener(this);
+
+        mDirectCallForMms = (SwitchPreference) findPreference(DIRECT_CALL_FOR_MMS);
+        mDirectCallForMms.setChecked((Settings.System.getInt(resolver,
+                Settings.System.DIRECT_CALL_FOR_MMS, 0) == 1));
+        mDirectCallForMms.setOnPreferenceChangeListener(this);
     }
 
     @Override
     public void onResume() {
         super.onResume();
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        ContentResolver resolver = getActivity().getContentResolver();
+        if (preference == mDirectCallForDialer) {
+            boolean value = (Boolean) newValue;
+            Settings.System.putInt(resolver, Settings.System.DIRECT_CALL_FOR_DIALER, value ? 1 : 0);
+            return true;
+        } else if (preference == mDirectCallForMms) {
+            boolean value = (Boolean) newValue;
+            Settings.System.putInt(resolver, Settings.System.DIRECT_CALL_FOR_MMS, value ? 1 : 0);
+            return true;
+        }
+        return false;
     }
 }
