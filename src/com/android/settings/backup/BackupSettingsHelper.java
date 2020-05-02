5bd7aa9d5ee9783edc7b1c11295ee65018020a5d
Wang Han <416810799@qq.com>
2020-05-02 17:08:18 +0800

    Settings: Ensure package is enabled in isIntentProvidedByTransport()
     * isIntentProvidedByTransport() does not check if the package is
       enabled, so we might face ActivityNotFoundException in
       UserBackupSettingsActivity later. Be robust even if this is not
       supported use case.
    
    Change-Id: I04deadbd24cd9e38fbe8b2b367b532a6dff3cdab

--- src/com/android/settings/backup/BackupSettingsHelper.java
+++ src/com/android/settings/backup/BackupSettingsHelper.java
@@ -19,8 +19,11 @@ package com.android.settings.backup;
 
 import android.app.backup.BackupManager;
 import android.app.backup.IBackupManager;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
 import android.os.RemoteException;
 import android.os.ServiceManager;
 import android.os.UserHandle;
@@ -182,7 +185,17 @@ public class BackupSettingsHelper {
     @VisibleForTesting
     boolean isIntentProvidedByTransport() {
         Intent intent = getIntentFromBackupTransport();
-        return intent != null && intent.resolveActivity(mContext.getPackageManager()) != null;
+        if (intent == null) {
+            return false;
+        }
+        ComponentName cn = intent.getComponent()
+        if (cn == null) {
+            return false;
+        }
+        String pkgName = cn.getPackageName();
+        final ApplicationInfo info = getApplicationInfo(mContext, pkgName, 0);
+        return info != null && info.enabled &&
+                intent.resolveActivity(mContext.getPackageManager()) != null;
     }
 
     /**
@@ -252,5 +265,17 @@ public class BackupSettingsHelper {
             Log.e(TAG, "Error getting data management summary", e);
         }
         return null;
+    }
+
+    private static ApplicationInfo getApplicationInfo(final Context context,
+                                                     final String packageName, final int flags) {
+        final PackageManager packageManager = mContext.getPackageManager();
+        ApplicationInfo info;
+        try {
+            info = packageManager.getApplicationInfo(packageName, flags);
+        } catch (PackageManager.NameNotFoundException e) {
+            info = null;
+        }
+        return info;
     }
 }
